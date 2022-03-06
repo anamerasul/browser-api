12adb063f6eeca0a337ed71c68f60698cff9595e
anamerasul <anamerasul@gmail.com>
2022-03-06 23:03:32 +0600

    Module Overview, BOM vs DOM, Browser API1~

---
+++ explore.js
@@ -0,0 +1,9 @@
+const name =15;
+const student={name:'jalil',friend:'borsha',job:'business',makeMovie:function(name){
+        console.log(name);
+}
+};
+student.makeMovie('din raat')
+// document.getElementById('name');
+
+// fetch(name)
