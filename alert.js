330da45ebc5ce5e90bf0460e5db4a1d29ea649c9
anamerasul <anamerasul@gmail.com>
2022-03-06 23:28:21 +0600

    Browser alert, confirm, prompt with examples

---
+++ alert.js
@@ -0,0 +1,29 @@
+// alert('abc');
+// alert('ma is coming open the book');
+const maCome=()=>{
+        alert('ma is coming open the book');
+}
+const askPicnic=()=>{
+        const response=confirm('are you going to picnic');
+
+        console.log(response)
+
+        if (response===true){
+                alert('fee ta bkash kor');
+        }
+
+        else{
+                console.log('dure giye mor')
+        }
+        
+}
+const askName=()=>{
+const name =prompt('what is your name');
+
+if(name){
+        console.log(name)
+}
+        
+}
+
+
