c31c07c9b730f8f03006e40eb989a9cbd91a7c36
anamerasul <anamerasul@gmail.com>
2022-03-09 03:23:51 +0600

    all

--- sumarry.js
+++ sumarry.js
@@ -4,4 +4,15 @@
 prompt('name');
 
 // url
-http://127.0.0.1:5500/shopping.html
+// http://127.0.0.1:5500/shopping.html
+
+// location.reload();
+// location.href ='';
+
+// location.hash=
+
+// history.back();
+// history.go();
+// localStorage vs sessionStorage
+
+// document.body.contentEditable=true
